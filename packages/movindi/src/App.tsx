bdccd068bf7021b58dbf187d9f61820af1130f2e
kimmeh <mhko0411@gmail.com>
2022-01-31 17:02:28 +0900

    add movindi signup views and router render

--- packages/movindi/src/App.tsx
+++ packages/movindi/src/App.tsx
@@ -3,15 +3,19 @@ import { ThemeProvider, createTheme } from '@material-ui/core';
 import { useRoutes } from 'react-router-dom';
 import './App.css';
 import Login from './views/Login';
-
+import Signup from './views/Signup';
 function App() {
   const routes = useRoutes([
     {
       path: '/login',
-      element: Login,
+      element: <Login />,
+    },
+    {
+      path: '/signup',
+      element: <Signup />,
     },
   ]);
-  return <>{routes}</>;
+  return routes;
 }
 
 export default App;
